f5f95725988afd1ff0099cf2d95506f470c6a981
NguyenThiThuyHNUE <nguyenthithuyqptb@gmail.com>
2021-07-07 17:00:25 +0700

    Tạo file letter.html

---
+++ Array/Demo.js
@@ -0,0 +1,7 @@
+// let arr_students= ["Nguyen Van A", "Nguyen Van B", "Nguyen Van C", "Nguyen Van D", "Nguyen Van E"]
+// console.log (arr_students[1]);
+let a = 3;
+let b = 4;
+let c = a+b;
+console.log (c);
+
