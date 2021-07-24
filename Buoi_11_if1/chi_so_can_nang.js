d1efc2fb77694dfcff21232702c7b70e0f4ee23d
NguyenThiThuyHNUE <nguyenthithuyqptb@gmail.com>
2021-07-24 21:53:15 +0700

    Buoi 12

--- Buoi_11_if1/chi_so_can_nang.js
+++ Buoi_11_if1/chi_so_can_nang.js
@@ -10,16 +10,16 @@ function kiem_tra() {
         result = "Gầy độ 2"
     else if (bmi < 18.5)
         result = "Gầy độ 1"
-    // else if (bmi < 25)
-    //     result = "Bình thường"
-    // else if (bmi < 30)
-    //     result = "Thừa cân"
-    // else if (bmi < 35)
-    //     result = "Béo phì độ 1" 
-    // else if (bmi < 40)
-    //     result = "Béo phì độ 2" 
-    // else
-    //     result = "Béo phì độ 3" + "<img src = 'https://i.pinimg.com/originals/73/4d/11/734d11cc677595ce3c3e5e3bba02721f.gif' width='100' height= '100'>"
+    else if (bmi < 25)
+        result = "Bình thường"
+    else if (bmi < 30)
+        result = "Thừa cân"
+    else if (bmi < 35)
+        result = "Béo phì độ 1" 
+    else if (bmi < 40)
+        result = "Béo phì độ 2" 
+    else
+        result = "Béo phì độ 3" + "<img src = 'https://i.pinimg.com/originals/73/4d/11/734d11cc677595ce3c3e5e3bba02721f.gif' width='100' height= '100'>"
 
     document.getElementById("result").innerHTML = "Chỉ số BMI là: " + bmi + ". Bạn " + "<b>"+ result + "</b>";
 }
--- Buoi_12_if2/tankpro.js
+++ Buoi_12_if2/tankpro.js
@@ -1,53 +1,82 @@
-        let top_tank = parseInt(document.getElementById("tank").style["top"]);
-        console.log(top_tank);
-        let left_tank = parseInt(document.getElementById("tank").style["left"]);
-        console.log(left_tank);
-        document.getElementById("tank").style["top"] =  top_tank - 19 + "px";
-        let top_boom = parseInt(document.getElementById("boom").style["top"]);
-        console.log(top_boom);
-        let left_boom = parseInt(document.getElementById("boom").style["left"]);
-        console.log(left_boom);
-
-
         
-      function explosion() {
-        let top_tank = parseInt(document.getElementById("tank").style["top"]);
-        let left_tank = parseInt(document.getElementById("tank").style["left"]);
-        let top_boom = ParseInt(document.getElementById("boom").style["top"]);
-        let left_boom = ParseInt(document.getElementById("boom").style["left"]);
+    let tank = document.getElementById("tank");
+    let boom = document.getElementById("boom");
+
+    function moveUp() {
+        let top_tank = parseInt(tank.style["top"]);
+        let left_tank = parseInt(tank.style["left"]);
+        let top_boom = parseInt(boom.style["top"]);
+        let left_boom = parseInt(boom.style["left"]);
+        tank.style["top"] =  top_tank - 19 + "px";
+
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
+
+        if (top_tank == top_boom && left_tank == left_boom) {
+            tank.style.backgroundImage = "url('img/explosion.png')"
+            tank.style.zIndex = 1;
+        }
+    }
+
+
+    function moveDown() {
+        let top_tank = parseInt(tank.style["top"]);
+        let left_tank = parseInt(tank.style["left"]);
+        let top_boom = parseInt(boom.style["top"]);
+        let left_boom = parseInt(boom.style["left"]);
+        tank.style["top"] =  top_tank + 19 + "px";
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
+
+        if (top_tank == top_boom && left_tank == left_boom) {
+            tank.style.backgroundImage = "url('img/explosion.png')"
+            tank.style.zIndex = 1;
+        }
+    }
+
+    function moveLeft() {
+        let top_tank = parseInt(tank.style["top"]);
+        let left_tank = parseInt(tank.style["left"]);
+        let top_boom = parseInt(boom.style["top"]);
+        let left_boom = parseInt(boom.style["left"]);
+        tank.style["left"] =  left_tank - 19 + "px";
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
+
+        if (top_tank == top_boom && left_tank == left_boom) {
+            tank.style.backgroundImage = "url('img/explosion.png')"
+            tank.style.zIndex = 1;
+        }
+    }
+
+    function moveRight() {
+        let top_tank = parseInt(tank.style["top"]);
+        let left_tank = parseInt(tank.style["left"]);
+        let top_boom = parseInt(boom.style["top"]);
+        let left_boom = parseInt(boom.style["left"]);
+        tank.style["left"] =  left_tank + 19 + "px";
+        top_tank = parseInt(tank.style["top"]);
+        left_tank = parseInt(tank.style["left"]);
+
+        if (top_tank == top_boom && left_tank == left_boom) {
+            tank.style.backgroundImage = "url('img/explosion.png')"
+            tank.style.zIndex = 1;
+        }
+    }
+
+
+
+    function explosion() {
+        let top_tank = parseInt(tank.style["top"]);
+        let left_tank = parseInt(tank.style["left"]);
+        let top_boom = ParseInt(boom.style["top"]);
+        let left_boom = ParseInt(boom.style["left"]);
         if (top_tank == top_boom && left_tank == left_boom) {
             document.getElementById("tank").style.backgroundImage="img/explosion.png"
         }
     }
     
-    function moveUp() {
-        let top_tank = parseInt(document.getElementById("tank").style["top"]);
-        console.log(top_tank);
-        let left_tank = parseInt(document.getElementById("tank").style["left"]);
-        console.log(left_tank);
-        document.getElementById("tank").style["top"] =  top_tank - 19 + "px";
-        let top_boom = parseInt(document.getElementById("boom").style["top"]);
-        console.log(top_boom);
-        let left_boom = parseInt(document.getElementById("boom").style["left"]);
-        console.log(left_boom);
 
-
-        if (top_tank == top_boom && left_tank == left_boom) {
-            document.getElementById("tank").style.backgroundImage = "url('img/explosion.png')"
-        }
-    }
-    function moveDown() {
-        let top_tank = parseInt(document.getElementById("tank").style["top"]);
-        document.getElementById("tank").style["top"] =  top_tank + 19 + "px";
-    }
-    function moveLeft() {
-        let left_tank = parseInt(document.getElementById("tank").style["left"]);
-        document.getElementById("tank").style["left"] = left_tank  - 19 + "px";
-    }
-    function moveRight() {
-        let left_tank = parseInt(document.getElementById("tank").style["left"]);
-        document.getElementById("tank").style["left"] =  left_tank + 19 + "px";
-    }
 
 
   
